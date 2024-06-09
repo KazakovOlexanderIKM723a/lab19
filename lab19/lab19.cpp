34970dea4ea4de1a402ae7a1a4f6b1807e7bfa35
Олександр Казаков <Oleksandr.Kazakov@infiz.khpi.edu.ua>
2024-06-09 18:24:46 +0300

    Добавьте файлы проекта.

---
+++ lab19/lab19.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+template <typename T>
+T nsd(T a, T b) {
+    const T epsilon = 1e-9;
+    while (abs(b) > epsilon) {
+        T temp = b;
+        b = fmod(a, b);
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    int a = 60;
+    int b = 288;
+    cout << "NSD (" << a << ", " << b << ") = " << nsd(a, b) << endl;
+
+    double x = 60.0;
+    double y = 288.0;
+    cout<< "NSD (" << x << ", " << y << ") = " << nsd(x, y) << endl;
+
+    return 0;
+}
+
+
+
